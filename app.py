c3b6ec5fc14dec39a26dfab0145fef628202ceed
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-01-25 03:23:54 +0530

    Add template Streamlit application

---
+++ app.py
@@ -0,0 +1,17 @@
+
+import streamlit as st
+
+st.set_page_config(page_title="QuCreate Streamlit Lab", layout="wide")
+st.sidebar.image("assets/images/company_logo.jpg")
+st.sidebar.divider()
+st.title("QuCreate Streamlit Lab")
+st.divider()
+
+# Code goes here
+st.write("Hello, Streamlit! You can start building your app here.")
+
+st.divider()
+st.write("© 2025 QuantUniversity. All Rights Reserved.")
+st.caption("The purpose of this demonstration is solely for educational use and illustration. "
+           "To access the full legal documentation, please visit this link. Any reproduction of this demonstration "
+           "requires prior written consent from QuantUniversity.")
